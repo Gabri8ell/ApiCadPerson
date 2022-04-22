a58080564a669f3720dc7991b94726c5fbc9a7f1
Gabri8ell <gbr192008@hotmail.com>
2022-04-22 14:19:00 -0300

    primeiro commit

---
+++ src/main/java/com/mypc/program/personaApi/DbPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.mypc.program.personaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypc/program/personaApi/DbPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypc.program.personaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
