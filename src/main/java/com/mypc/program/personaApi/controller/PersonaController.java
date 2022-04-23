bec9ab4d146bfee1bfbfa32080fdbfe1f8e454f9
Gabri8ell <gbr192008@hotmail.com>
2022-04-22 21:30:13 -0300

    commit teste

--- src/main/java/com/mypc/program/personaApi/controller/PersonaController.java
+++ src/main/java/com/mypc/program/personaApi/controller/PersonaController.java
@@ -1,4 +1,4 @@
-package com.mypc.program.personaApi;
+package com.mypc.program.personaApi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
