d22b2900999bdfb91870f2b51f6f5bbc02f3adb3
Gabri8ell <gbr192008@hotmail.com>
2022-04-22 21:52:26 -0300

    commit add @ComponentScan

--- src/main/java/com/mypc/program/personaApi/DbPersonaApplication.java
+++ src/main/java/com/mypc/program/personaApi/DbPersonaApplication.java
@@ -2,8 +2,10 @@ package com.mypc.program.personaApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com/mypc/program/personaApi/controller"})
 public class DbPersonaApplication {
 
 	public static void main(String[] args) {
