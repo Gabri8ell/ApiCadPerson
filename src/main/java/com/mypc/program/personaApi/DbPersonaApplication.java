6bd3afa0b39ae312c730b972236c90740ed73b34
Gabri8ell <gbr192008@hotmail.com>
2022-04-22 22:09:11 -0300

    remove @ComponentScan

--- src/main/java/com/mypc/program/personaApi/DbPersonaApplication.java
+++ src/main/java/com/mypc/program/personaApi/DbPersonaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com/mypc/program/personaApi/controller"})
+//@ComponentScan(basePackages = {"com/mypc/program/personaApi/controller"})
 public class DbPersonaApplication {
 
 	public static void main(String[] args) {
