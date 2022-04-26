2c41acde7807ee02fb45db175fa6d7a763025a11
Gabri8ell <gbr192008@hotmail.com>
2022-04-26 01:19:13 -0300

    Add Get by Id and Get All

--- src/main/java/com/mypc/program/personaApi/controller/PersonaController.java
+++ src/main/java/com/mypc/program/personaApi/controller/PersonaController.java
@@ -2,12 +2,14 @@ package com.mypc.program.personaApi.controller;
 
 import com.mypc.program.personaApi.dto.response.MessageResponseDTO;
 import com.mypc.program.personaApi.dto.request.PersonDTO;
+import com.mypc.program.personaApi.exception.PersonNotFoundException;
 import com.mypc.program.personaApi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -25,6 +27,16 @@ public class PersonaController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
 
+    }
+
+    @GetMapping
+    public List<PersonDTO> ListAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 
 }
--- src/main/java/com/mypc/program/personaApi/services/PersonService.java
+++ src/main/java/com/mypc/program/personaApi/services/PersonService.java
@@ -2,10 +2,15 @@ package com.mypc.program.personaApi.services;
 import com.mypc.program.personaApi.dto.response.MessageResponseDTO;
 import com.mypc.program.personaApi.dto.request.PersonDTO;
 import com.mypc.program.personaApi.entity.Person;
+import com.mypc.program.personaApi.exception.PersonNotFoundException;
 import com.mypc.program.personaApi.mapper.PersonMapper;
 import com.mypc.program.personaApi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -28,4 +33,18 @@ public class PersonService {
                 .build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(()-> new PersonNotFoundException(id));
+
+        return personMapper.toDTO(person);
+    }
 }
