3de0a0164e60fe9d3cde8450176c0f7eea03167c
Gabri8ell <gbr192008@hotmail.com>
2022-04-29 00:53:07 -0300

    Add method putById

--- src/main/java/com/mypc/program/personaApi/controller/PersonController.java
+++ src/main/java/com/mypc/program/personaApi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.mypc.program.personaApi.dto.response.MessageResponseDTO;
 import com.mypc.program.personaApi.dto.request.PersonDTO;
 import com.mypc.program.personaApi.exception.PersonNotFoundException;
 import com.mypc.program.personaApi.services.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+//@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonaController {
 
     private PersonService personService;
@@ -38,5 +40,17 @@ public class PersonaController {
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateByID(@PathVariable Long id, @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
 
 }
